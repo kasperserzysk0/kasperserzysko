d21f116dbd482de177bc77b33fd3d54e24cab5e1
kasperserzysk0 <s24270@pjwstk.edu.pl>
2021-10-08 01:01:34 +0200

    error fixed

--- hello.cpp
+++ hello.cpp
@@ -2,12 +2,9 @@
 
 auto main() -> int
 {
-	cout << "GOOD MORNING WORLD\n";
+	std::cout << "GOOD MORNING WORLD\n";
 	int a;
-	cin>>a;
-	cout <<a<<"\n";
-	
-
-    return 0;
+	std::cin>>a;
+	std::cout <<a<<"\n";
 }
 
